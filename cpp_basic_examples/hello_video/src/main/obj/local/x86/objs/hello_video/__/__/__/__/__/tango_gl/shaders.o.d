9f4c451462717c3f5f75edd9c41f3e40644bea03
Alexandra Kiba <akiba@kraftway.ru>
2017-06-28 12:51:36 +0300

    remove libs and objs, change name

--- cpp_basic_examples/hello_video/src/main/obj/local/x86/objs/hello_video/__/__/__/__/__/tango_gl/shaders.o.d
+++
@@ -1,8 +0,0 @@
-/home/dreik/repos/tango-examples-c/cpp_basic_examples/hello_video/src/main/obj/local/x86/objs/hello_video/__/__/__/__/__/tango_gl/shaders.o: \
-  /home/dreik/repos/tango-examples-c/cpp_basic_examples/hello_video/src/main/jni/../../../../../tango_gl/shaders.cc \
-  /home/dreik/repos/tango-examples-c/cpp_basic_examples/hello_video/src/main/jni/../../../../../tango_gl/include/tango-gl/shaders.h \
-  /home/dreik/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string
-
-/home/dreik/repos/tango-examples-c/cpp_basic_examples/hello_video/src/main/jni/../../../../../tango_gl/include/tango-gl/shaders.h:
-
-/home/dreik/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string:
